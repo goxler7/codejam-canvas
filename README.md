a8eb36a2c906517f1ef03fdac1072659063a482a
goxler7 <GOXLER.7@GMAIL.COM>
2019-10-27 12:20:46 +0700

    feat: add basic layout

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-### Codejam canvas
+# Codejam canvas
+
+### How to use it:
+1. Clone or download my repository 
+2. Install dependencies ```npm install``` 
+3. Start server at http://localhost:8081/ ```npm run dev``` 
+4. Build project for output files at dist folder ```npm run build```
---
+++ src/index.js
@@ -0,0 +1,46 @@
+import './assets/scss/main.scss'
+
+let img4x4 = require('./data/4x4.json');
+let img32x32 = require('./data/32x32.json');
+
+let imgRs = new Image()
+imgRs.src = './data/image.png';
+
+const btn4x4 = document.querySelector('.btn_4x4');
+const btn32x32 = document.querySelector('.btn_32x32');
+const btnRS = document.querySelector('.btn_rs');
+
+let ctx = canvas.getContext('2d');
+
+function draw(img) {
+  if(!img.src) {
+    canvas.width = img.length;
+    canvas.height = img.length;
+    for (let i = 0; i < img.length; i++) {
+      for (let j = 0; j < img.length; j++) {
+        if (typeof(img[i][j]) === 'string') {
+          ctx.fillStyle = `#${img[i][j]}`;
+        } else {
+          ctx.fillStyle = `rgba(${img[i][j][0]},${img[i][j][1]},${img[i][j][2]},${img[i][j][3]})`
+        }
+        ctx.fillRect(i, j , 1, 1);
+      }
+    } 
+  } else {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+  }
+}
+
+btnRS.addEventListener('click', function() {
+  draw(imgRs)
+});
+
+btn4x4.addEventListener('click', function(){
+  draw(img4x4)
+});
+
+btn32x32.addEventListener('click', function(){
+  draw(img32x32)
+});
